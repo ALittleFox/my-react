cadd1891d102b9f5659334847d078ba0f7080c7d
fox <zxz@yonyou.com>
2023-03-15 00:20:56 +0800

    feat: add tsconfig.json

---
+++ packages/test.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: zhouxc
+ * @LastEditors: zhouxc
+ * @Email: zhouxz@yonyou.com
+ * @Date: 2023-03-14 23:57:27
+ * @LastEditTime: 2023-03-14 23:57:39
+ * @Description: $0
+ */
+const a = 123
